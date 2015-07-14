825827c3bf83a9cd519fc72574b3dbd3ab4dd31c
shlomitArgov <shlomit.argov@gmail.com>
2015-07-14 22:28:20 +0300

    Added implementation of link from the more_information dialog menu to the MOMA NYC website

--- app/src/main/java/com/shlomitargov/modernartui/MoreInformationFragment.java
+++ app/src/main/java/com/shlomitargov/modernartui/MoreInformationFragment.java
@@ -1,15 +1,20 @@
 package com.shlomitargov.modernartui;
 
+import android.app.Activity;
 import android.app.AlertDialog;
 import android.app.Dialog;
 import android.app.DialogFragment;
 import android.content.DialogInterface;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 
 /**
  * Created by Shlomit on 14-Jul-15.
  */
 public class MoreInformationFragment extends DialogFragment {
+    private static final String MOMA_NYC_URL = "http://www.moma.org";
+
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
         AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
@@ -17,6 +22,11 @@ public class MoreInformationFragment extends DialogFragment {
                 .setPositiveButton(R.string.visit_moma, new DialogInterface.OnClickListener() {
                     public void onClick(DialogInterface dialog, int id) {
                         //Go to MOMA NYC site
+                        Intent momaIntent = new Intent();
+                        momaIntent.setData(Uri.parse(MOMA_NYC_URL));
+                        momaIntent.setAction(Intent.ACTION_VIEW);
+                        momaIntent.addCategory(Intent.CATEGORY_BROWSABLE);
+                        startActivity(momaIntent);
                     }
                 })
                 .setNegativeButton(R.string.not_now, new DialogInterface.OnClickListener() {
