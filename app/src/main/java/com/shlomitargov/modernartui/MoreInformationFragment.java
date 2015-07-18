4761b2bd5962804dcd2fc3d69745a90b9bd55eba
shlomitArgov <shlomit.argov@gmail.com>
2015-07-18 12:40:14 +0300

    Removed unused fragmant layout file. Formatted text in dialog.

--- app/src/main/java/com/shlomitargov/modernartui/MoreInformationFragment.java
+++ app/src/main/java/com/shlomitargov/modernartui/MoreInformationFragment.java
@@ -4,10 +4,20 @@ import android.app.Activity;
 import android.app.AlertDialog;
 import android.app.Dialog;
 import android.app.DialogFragment;
+import android.app.Fragment;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.text.Html;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AbsListView;
+import android.widget.FrameLayout;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 /**
  * Created by Shlomit on 14-Jul-15.
@@ -18,9 +28,12 @@ public class MoreInformationFragment extends DialogFragment {
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
         AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
-        builder.setMessage(R.string.dialog_text)
-                .setPositiveButton(R.string.visit_moma, new DialogInterface.OnClickListener() {
-                    public void onClick(DialogInterface dialog, int id) {
+
+        builder.setMessage(Html.fromHtml(getString(R.string.dialog_text)))
+                .setPositiveButton(R.string.visit_moma, new DialogInterface.OnClickListener()
+                {
+                    public void onClick(DialogInterface dialog, int id)
+                    {
                         //Go to MOMA NYC site
                         Intent momaIntent = new Intent();
                         momaIntent.setData(Uri.parse(MOMA_NYC_URL));
@@ -29,11 +42,14 @@ public class MoreInformationFragment extends DialogFragment {
                         startActivity(momaIntent);
                     }
                 })
-                .setNegativeButton(R.string.not_now, new DialogInterface.OnClickListener() {
-                    public void onClick(DialogInterface dialog, int id) {
+                .setNegativeButton(R.string.not_now, new DialogInterface.OnClickListener()
+                {
+                    public void onClick(DialogInterface dialog, int id)
+                    {
                         // User cancelled the dialog - do nothing
                     }
                 });
+
         // Create the AlertDialog object and return it
         return builder.create();
     }
